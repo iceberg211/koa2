3bd9730d20ab91a018310c0a0a8942134c2366d1
何炜 <hewei@77ircloud.com>
2018-06-21 20:25:43 +0800

    修改readMe

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+###
+
+koa2 电影网站
+
+
 ```
 const Koa = require('koa');
 const app = new Koa();
---
+++ src/views/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class app extends Component {
+  render() {
+    return (
+      <div>
+        react 加载 测试
+      </div>
+    )
+  }
+}
